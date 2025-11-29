02731439fc020de47462249b4262092b84810adc
Mr Mekis <maxell.nathanael@gmail.com>
2025-11-29 16:08:38 +0700

    Commit No. 3 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.test_lab_week_12
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.test_lab_week_12.model.Movie
@@ -9,31 +8,40 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import java.util.Calendar
 
-class MovieViewModel(private val movieRepository: MovieRepository)
-    : ViewModel() {
+class MovieViewModel(
+    private val movieRepository: MovieRepository
+) : ViewModel() {
+
+    // StateFlow untuk list movie
+    private val _popularMovies = MutableStateFlow<List<Movie>>(emptyList())
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
+    // StateFlow untuk error
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
     init {
         fetchPopularMovies()
     }
-    private val _popularMovies = MutableStateFlow(
-        emptyList<Movie>()
-    )
-    val popularMovies: StateFlow<List<Movie>> = _popularMovies
-    private val _error = MutableStateFlow("")
-    val error: StateFlow<String> = _error
-    // fetch movies from the API
+
     private fun fetchPopularMovies() {
-// launch a coroutine in viewModelScope
-// Dispatchers.IO means that this coroutine will run on a shared pool of threads
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies().catch {
-// catch is a terminal operator that catches exceptions from the Flow
-                _error.value = "An exception occurred: ${it.message}"
-            }.collect {
-// collect is a terminal operator that collects the values from the Flow
-// the results are emitted to the StateFlow
-                _popularMovies.value = it
-            }
+            movieRepository.fetchMovies()
+                .catch { throwable ->
+                    _error.value = "An exception occurred: ${throwable.message}"
+                }
+                .collect { movies ->
+                    val currentYear =
+                        Calendar.getInstance().get(Calendar.YEAR).toString()
+
+                    _popularMovies.value = movies
+                        .filter { movie ->
+                            movie.releaseDate?.startsWith(currentYear) == true
+                        }
+                        .sortedByDescending { it.popularity }
+                }
         }
     }
 }
